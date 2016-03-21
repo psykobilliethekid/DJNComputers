e47dcc02ccf026b5304da695673956b135efca50
Danielle "Dani" Moss <dani.j.moss@gmail.com>
2016-03-21 17:09:31 -0400

    Added core files and images

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	//Image Gallery
+	//Rotates every 10 seconds
+    $('#Carousel').carousel({
+        interval: 10000
+    })
+
+
+    //Bootsnipp: Bootstrap Lightbox
+    //http://bootsnipp.com/snippets/featured/bootstrap-lightbox
+    //Lightbox for Image Gallery
+    var $lightbox = $('#lightbox');
+    
+    $('[data-target="#lightbox"]').on('click', function(event) {
+        var $img = $(this).find('img'), 
+            src = $img.attr('src'),
+            alt = $img.attr('alt'),
+            css = {
+                'maxWidth': $(window).width() - 100,
+                'maxHeight': $(window).height() - 100
+            };
+    
+        $lightbox.find('.close').addClass('hidden');
+        $lightbox.find('img').attr('src', src);
+        $lightbox.find('img').attr('alt', alt);
+        $lightbox.find('img').css(css);
+    });
+    
+    $lightbox.on('shown.bs.modal', function (e) {
+        var $img = $lightbox.find('img');
+            
+        $lightbox.find('.modal-dialog').css({'width': $img.width()});
+        $lightbox.find('.close').removeClass('hidden');
+    });
+});
